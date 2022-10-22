47ffcd2b28387b0d3e4a4c0c1869b4850bdbf2bb
UlrichBR <38494475+UlrichBR@users.noreply.github.com>
2022-10-22 10:56:51 -0300

    Add files via upload

---
+++ src/main/java/me/ulrich/koth/data/OptimalTextLine.java
@@ -0,0 +1,36 @@
+package me.ulrich.koth.data;
+
+
+import org.bukkit.ChatColor;
+
+import me.ulrich.koth.interfaces.TextLine;
+
+
+public final class OptimalTextLine implements TextLine {
+
+    private final OptimalHologram hologram;
+
+    private String text;
+
+    /**
+     * Create a new Item Line
+     *
+     * @param hologram The parent {@link OptimalHologram}
+     * @param text     The default {@link String} text
+     */
+    public OptimalTextLine(OptimalHologram hologram, String text) {
+        this.hologram = hologram;
+        this.text = ChatColor.translateAlternateColorCodes('&', text);
+    }
+
+    public String getText() {
+        return this.text;
+    }
+
+    @Override
+    public void setText(String text) {
+        this.text = ChatColor.translateAlternateColorCodes('&', text);
+        this.updateHologram(hologram);
+    }
+
+}
