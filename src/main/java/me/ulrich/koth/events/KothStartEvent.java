054d81f34d2f58b2649d6823d027663e867f7a77
UlrichBR <38494475+UlrichBR@users.noreply.github.com>
2023-03-05 19:16:20 -0300

    Create KothStartEvent.java

---
+++ src/main/java/me/ulrich/koth/events/KothStartEvent.java
@@ -0,0 +1,46 @@
+package me.ulrich.koth.events;
+
+import java.util.UUID;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+public class KothStartEvent extends Event implements Cancellable {
+
+	private static final HandlerList handlers = new HandlerList();
+    private boolean cancelled;
+	private UUID kothUUID;
+    
+    public KothStartEvent(UUID kothUUID, Player player) {
+    	this.setKothUUID(kothUUID);
+    }
+    
+	public boolean isCancelled() {
+		return cancelled;
+	}
+
+	public void setCancelled(boolean cancelled) {
+		this.cancelled = cancelled;
+	}
+	
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+
+	public UUID getKothUUID() {
+		return kothUUID;
+	}
+
+	public void setKothUUID(UUID kothUUID) {
+		this.kothUUID = kothUUID;
+	}
+
+
+
+}
