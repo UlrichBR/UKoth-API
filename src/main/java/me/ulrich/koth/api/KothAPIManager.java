62bee8f7863cc3defe8b41cd54dcd39ed621e2a5
UlrichBR <38494475+UlrichBR@users.noreply.github.com>
2023-03-05 20:48:54 -0300

    Update KothAPIManager.java

--- src/main/java/me/ulrich/koth/api/KothAPIManager.java
+++ src/main/java/me/ulrich/koth/api/KothAPIManager.java
@@ -1,4 +1,4 @@
-package me.ulrich.koth.api;
+package me.ulrich.koth.interfaces;
 
 import java.util.HashMap;
 import java.util.List;
@@ -8,130 +8,62 @@ import java.util.UUID;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
-import me.ulrich.koth.Koth;
 import me.ulrich.koth.data.CubRegionLoc;
 import me.ulrich.koth.data.IslandJoinLeaveData;
 import me.ulrich.koth.data.KothData;
-import me.ulrich.koth.interfaces.GroupAPI;
-import me.ulrich.koth.interfaces.HologramAPI;
-import me.ulrich.koth.interfaces.KothAPI;
-import me.ulrich.koth.interfaces.ScoreboardAPI;
 
-public class KothAPIManager implements KothAPI {
+public interface KothAPI {
 
-	public static KothAPIManager getInstance() {
-		return Koth.getCore().getKothAPI();
-	}
+	void addGroupImplement(GroupAPI impl);
 	
-	@Override
-	public String getTag() {
-		return null;
-	}
+	boolean hasGroupImplemented();
+
+	void addScoreboardImplement(ScoreboardAPI impl);
 	
-	@Override
-	public boolean hasHoloImplemented() {
-		return false;
-	}
+	boolean hasScoreboardImplemented();
+
+	void addHoloImplement(HologramAPI impl);
 	
-	@Override
-	public void addHoloImplement(HologramAPI impl) {}
+	boolean hasHoloImplemented();
 	
-	@Override
-	public boolean hasGroupImplemented() {
-		return false;
-	}
-	
-	@Override
-	public void addGroupImplement(GroupAPI impl) {}
-	
-	@Override
-	public boolean hasScoreboardImplemented() {
-		return false;
-	}
-	
-	@Override
-	public void addScoreboardImplement(ScoreboardAPI impl) {}
-	
-	@Override
-	public boolean startKothEvent(String kothName) {
-		return false;
-	}
-	
-	@Override
-	public boolean startKothEvent(UUID kothUUID) {
-		return false;
-	}
-	
-	@Override
-	public boolean startKothEvent(UUID kothUUID, Player sender) {
-		return false;
-	}
-	
-	@Override
-	public boolean endKothEvent(String kothName) {
-		return false;
-	}
-	
-	@Override
-	public boolean endKothEvent(UUID kothUUID) {
-		return false;
-	}
-	
-	@Override
-	public boolean endKothEvent(UUID kothUUID, Player winner) {
-		return false;
-	}
-	
-	@Override
-	public boolean hasKothStarted(String kothName) {
-		return false;
-	}
-	
-	@Override
-	public boolean hasKothStarted(UUID kothUUID) {
-		return false;
-	}
-	
-	@Override
-	public String getKothName(UUID kothUUID) {
-		return null;
-	}
-	
-	@Override
-	public UUID getKothUUID(String kothName) {
-		return null;
-	}
-	
-	@Override
-	public KothData getKoth(UUID kothUUID) {
-		return null;
-	}
-	
-	@Override
-	public CubRegionLoc getKoth(Location location) {
-		return null;
-	}
-	
-	@Override
-	public List<Player> getAdminMode() {
-		return null;
-	}
-	
-	@Override
-	public HashMap<UUID, IslandJoinLeaveData> getPlayerRegion() {
-		return null;
-	}
-	
-	@Override
-	public HashMap<UUID, KothData> getKothData() {
-		return null;
-	}
-	
-	@Override
-	public Set<UUID> getAllKothEvent(){
-		return null;
-	}
-	
+	boolean startKothEvent(String kothName);
+
+	boolean startKothEvent(UUID kothUUID);
+
+	boolean endKothEvent(String kothName);
+
+	boolean endKothEvent(UUID kothUUID);
+
+	boolean endKothEvent(UUID kothUUID, Player winner);
+
+	Set<UUID> getAllKothEvent();
+
+	boolean hasKothStarted(String kothName);
+
+	boolean hasKothStarted(UUID kothUUID);
+
+	UUID getKothUUID(String kothName);
+
+	String getKothName(UUID kothUUID);
+
+	String getTag();
+
+	KothData getKoth(UUID kothUUID);
+
+	CubRegionLoc getKoth(Location location);
+
+	List<Player> getAdminMode();
+
+	HashMap<UUID, IslandJoinLeaveData> getPlayerRegion();
+
+	HashMap<UUID, KothData> getKothData();
+
+	boolean startKothEvent(UUID kothUUID, Player sender);
+
+	String parseText(KothData koth, String text);
+
+	String parseText(UUID playerUUID, String text);
+
 	
 
 
